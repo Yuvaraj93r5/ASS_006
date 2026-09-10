1b1c50a3de99a9d1be830afc61773642ffbaf182
Yuvaraj93r5 <yuvaraj.g2024@vitstudent.ac.in>
2026-09-10 19:04:39 +0530

    Replace ViolationType with VehicleType enum

--- src/main/java/com/system/model/VehicleType.java
+++ src/main/java/com/system/model/VehicleType.java
@@ -1,17 +1,8 @@
 package com.system.model;
 
-public enum ViolationType {
-    OVER_SPEEDING(2000.0),
-    SIGNAL_VIOLATION(1500.0),
-    ILLEGAL_PARKING(1000.0);
-
-    private final double baseFine;
-
-    ViolationType(double baseFine) {
-        this.baseFine = baseFine;
-    }
-
-    public double getBaseFine() {
-        return baseFine;
-    }
+public enum VehicleType {
+    TWO_WHEELER,
+    FOUR_WHEELER,
+    COMMERCIAL
 }
+
